2f5c005b837722d4adcf88b3914eb816e0306d69
jkinyanjui <joseph.kinyanjui@codecentric.de>
2025-01-28 10:31:00 +0100

    sol 6.extra-1. useRef hook

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,18 +2,19 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import {useRef} from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const inputRef = useRef(null)
   function handleSubmit(event) {
-    event.preventDefault()
-    onSubmitUsername(event.target.elements.username.value)
+    onSubmitUsername(inputRef.current.value)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" ref={inputRef}/>
       </div>
       <button type="submit">Submit</button>
     </form>
